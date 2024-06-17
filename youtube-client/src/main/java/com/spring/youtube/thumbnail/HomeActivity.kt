ddad231699c651016e6d43c0e4c3083cc8e4ae42
spring98 <foreverspring98@gmail.com>
2024-06-18 00:00:59 +0900

    Feature: server json, image, video api 구현

--- youtube-client/src/main/java/com/spring/youtube/thumbnail/HomeActivity.kt
+++ youtube-client/src/main/java/com/spring/youtube/thumbnail/HomeActivity.kt
@@ -1,10 +1,13 @@
 package com.spring.youtube.thumbnail
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
+import com.google.gson.Gson
 import com.spring.youtube.thumbnail.utils.Server
 import com.spring.youtube.thumbnail.utils.Server.Companion.url
 import kotlinx.coroutines.*
@@ -27,23 +30,45 @@ class HomeActivity : AppCompatActivity() {
         webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val htmlData = fetchHtmlFromServer()
-            runOnUiThread {
-                webView.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null)
+//        CoroutineScope(Dispatchers.IO).launch {
+//            val htmlData = fetchHtmlFromServer()
+//            runOnUiThread {
+//                webView.loadDataWithBaseURL(null, htmlData, "text/html", "UTF-8", null)
+//            }
+//        }
+
+        val button: Button = findViewById(R.id.serverBtn)
+        val jsonButton: Button = findViewById(R.id.jsonBtn)
+        val imageButton: Button = findViewById(R.id.imageBtn)
+        val videoButton: Button = findViewById(R.id.videoBtn)
+
+        button.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                startClient()
             }
         }
 
-//        val button: Button = findViewById(R.id.serverBtn)
-//        button.setOnClickListener {
-//            GlobalScope.launch(Dispatchers.IO) {
-//                startClient()
-//            }
-//        }
+        jsonButton.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                fetchJsonFromServer()
+            }
+        }
+
+        imageButton.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                fetchImageFromServer()
+            }
+        }
+
+        videoButton.setOnClickListener {
+            GlobalScope.launch(Dispatchers.IO) {
+                fetchVideoFromServer()
+            }
+        }
     }
 
     private fun startClient() {
-        val serverIp = Server.url  // 서버 IP 주소
+        val serverIp = url  // 서버 IP 주소
         val serverPort = 1234  // 서버 포트
 
         try {
@@ -74,7 +99,7 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun fetchHtmlFromServer(): String {
-        val urlString = "http://${Server.url}:1234"  // URL에 프로토콜 추가
+        val urlString = "http://$url:1234"  // URL에 프로토콜 추가
         val url = URL(urlString)
         val connection = url.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
@@ -99,4 +124,108 @@ class HomeActivity : AppCompatActivity() {
             throw Exception("Failed to fetch HTML: HTTP response code $responseCode")
         }
     }
+
+    private suspend fun fetchJsonFromServer() {
+        val urlString = "http://$url:1234/json"
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+
+        try {
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                val response = reader.readText()
+                reader.close()
+                inputStream.close()
+
+                val responseData = Gson().fromJson(response, ResponseData::class.java)
+                withContext(Dispatchers.Main) {
+                    webView.loadDataWithBaseURL(null, """
+                        <html>
+                        <body>
+                        <h1>${responseData.message}</h1>
+                        <p>Client IP: ${responseData.clientIp}</p>
+                        <p>Client Port: ${responseData.clientPort}</p>
+                        </body>
+                        </html>
+                    """.trimIndent(), "text/html", "UTF-8", null)
+                }
+            } else {
+                Log.e("HomeActivity", "Failed to fetch JSON: HTTP response code $responseCode")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            connection.disconnect()
+        }
+    }
+
+    private fun fetchImageFromServer() {
+        val urlString = "http://$url:1234/image"
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+
+        try {
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                Log.d("spring", inputStream.toString())
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                Log.d("spring", inputStream.toString())
+
+                inputStream.close()
+
+                Log.d("spring", bitmap.toString())
+
+//                withContext(Dispatchers.Main) {
+//                    imageView.setImageBitmap(bitmap)
+//                }
+            } else {
+                Log.e("HomeActivity", "Failed to fetch image: HTTP response code $responseCode")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            connection.disconnect()
+        }
+    }
+
+    private suspend fun fetchVideoFromServer() {
+        val urlString = "http://$url:1234/video"
+        val url = URL(urlString)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+
+        try {
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val videoBytes = inputStream.readBytes()
+                inputStream.close()
+
+                withContext(Dispatchers.Main) {
+                    // Here, you can save the video to a file and play it using a VideoView
+                    // For simplicity, we will log the size of the video data
+                    Log.d("HomeActivity", "Fetched video with size: ${videoBytes.size} bytes")
+                }
+            } else {
+                Log.e("HomeActivity", "Failed to fetch video: HTTP response code $responseCode")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            connection.disconnect()
+        }
+    }
 }
+
+data class ResponseData(val message: String, val clientIp: String, val clientPort: Int)
--- youtube-server/src/main/java/com/spring/youtube/server/HostingService.kt
+++ youtube-server/src/main/java/com/spring/youtube/server/HostingService.kt
@@ -9,15 +9,17 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.google.gson.Gson
 import java.io.BufferedReader
+import java.io.InputStream
 import java.io.InputStreamReader
+import java.io.OutputStream
 import java.net.*
 import kotlin.concurrent.thread
 
 class HostingService : Service() {
+    private val tag = "spring-main"
     private val CHANNELID = "Foreground Service ID"
-    private var clientAddress = ""
-    private var clientPort = 0
     private lateinit var notificationManager: NotificationManager
     private val BROADCAST_PORT = 12345
     private val SERVER_PORT = 1234
@@ -27,7 +29,6 @@ class HostingService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
         thread {
             startServer()
         }
@@ -44,7 +45,7 @@ class HostingService : Service() {
 
     private fun startServer() {
         val server = ServerSocket(SERVER_PORT)
-        Log.e("Service", "Server started on port $SERVER_PORT")
+        Log.d(tag, "Server started on port $SERVER_PORT")
 
         while (true) {
             val client: Socket = server.accept()
@@ -55,66 +56,123 @@ class HostingService : Service() {
 //    private fun handleClient(client: Socket) {
 //        client.use { socket ->
 //            val output = socket.getOutputStream()
+//            val input = socket.getInputStream()
+//            val reader = BufferedReader(InputStreamReader(input))
 //
-//            clientAddress = socket.inetAddress.hostAddress ?: ""
-//            clientPort = socket.port
+//            val clientAddress = socket.inetAddress.hostAddress ?: ""
+//            val clientPort = socket.port
 //
 //            updateNotification("유저 접속: $clientAddress:$clientPort")
-//            Log.e("Service", "Client connected: $clientAddress:$clientPort")
+//            Log.d(tag, "Client connected: $clientAddress:$clientPort")
 //
-//            // HTTP Response
-//            val response = """
+//            try {
+//                // Read the request
+//                val requestLine = reader.readLine()
+//                Log.e(tag, "Received request: $requestLine")
+//                Log.e(tag, "Server started on port $SERVER_PORT")
+//
+//                /**
+//                 * Header
+//                 *  1. General Header
+//                 *      1. Date
+//                 *      2. Connection
+//                 *          close: 메세지 교환 후 TCP 연결 종료
+//                 *          Keep-Alive: 메세지 교환 후 TCP 연결 유지
+//                 *
+//                 *  2. Request Header
+//                 *      1. Host
+//                 *      2. User-Agent
+//                 *      3. Accept
+//                 *      4. Cookie
+//                 *      5. Referer
+//                 *
+//                 *  3. Response Header
+//                 *      1. Server
+//                 *      2. content-encoding
+//                 *      3. content-type
+//                 *      4. cache-control
+//                 *      5. date
+//                 *      6. vary
+//                 *      7. Set-Cookie
+//                 *      8. Age
+//                 */
+//
+////                // HTTP Response
+////                val response = """
+////                        HTTP/1.1 200 OK
+////                        Content-Type: text/html
+////                        Connection: close
+////
+////                        <html>
+////                        <body>
+////                        <h1>Hello, World!</h1>
+////                        <p>Client IP: $clientAddress</p>
+////                        <p>Client Port: $clientPort</p>
+////                        </body>
+////                        </html>
+////                    """.trimIndent()
+////
+////                output.write(response.toByteArray())
+////                output.flush()
+//
+//                // Load image from assets
+//                val imageStream: InputStream = assets.open("waffle-preview-image.png")
+//                val imageBytes = imageStream.readBytes()
+//                imageStream.close()
+//
+//                // HTTP Response
+//                val responseHeader = """
 //                HTTP/1.1 200 OK
-//                Content-Type: text/html
+//                Content-Type: image/png
+//                Content-Length: ${imageBytes.size}
 //                Connection: close
 //
-//                <html>
-//                <body>
-//                <h1>Hello, World!</h1>
-//                <p>Client IP: $clientAddress</p>
-//                <p>Client Port: $clientPort</p>
-//                </body>
-//                </html>
 //            """.trimIndent()
 //
-//            output.write(response.toByteArray())
-//            output.flush()
+//                output.write(responseHeader.toByteArray())
+//                output.write(imageBytes)
+//                output.flush()
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            } finally {
+//                input.close()
+//                output.close()
+//                socket.close()
+//            }
 //        }
 //    }
+
     private fun handleClient(client: Socket) {
         client.use { socket ->
             val output = socket.getOutputStream()
             val input = socket.getInputStream()
             val reader = BufferedReader(InputStreamReader(input))
 
-            clientAddress = socket.inetAddress.hostAddress ?: ""
-            clientPort = socket.port
+            val clientAddress = socket.inetAddress.hostAddress ?: ""
+            val clientPort = socket.port
 
             updateNotification("유저 접속: $clientAddress:$clientPort")
-            Log.e("Service", "Client connected: $clientAddress:$clientPort")
+            Log.d(tag, "Client connected: $clientAddress:$clientPort")
 
             try {
                 // Read the request
                 val requestLine = reader.readLine()
-                Log.e("Service", "Received request: $requestLine")
+                Log.d(tag, "Received request: $requestLine")
 
-                // HTTP Response
-                val response = """
-                        HTTP/1.1 200 OK
-                        Content-Type: text/html
-                        Connection: close
-    
-                        <html>
-                        <body>
-                        <h1>Hello, World!</h1>
-                        <p>Client IP: $clientAddress</p>
-                        <p>Client Port: $clientPort</p>
-                        </body>
-                        </html>
-                    """.trimIndent()
+                val requestParts = requestLine.split(" ")
+                if (requestParts.size < 2) return
 
-                output.write(response.toByteArray())
-                output.flush()
+                Log.d(tag, requestParts.toString())
+
+                val method = requestParts[0]
+                val path = requestParts[1]
+
+                when (path) {
+                    "/json" -> sendJsonResponse(output, clientAddress, clientPort)
+                    "/image" -> sendImageResponse(output, "waffle-preview-image.png")
+                    "/video" -> sendVideoResponse(output, "waffle-preview-video.mp4")
+                    else -> sendNotFoundResponse(output)
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
             } finally {
@@ -123,6 +181,82 @@ class HostingService : Service() {
                 socket.close()
             }
         }
+    }
+
+    private fun sendJsonResponse(output: OutputStream, clientAddress: String, clientPort: Int) {
+        val responseData = ResponseData("예뻤어.", clientAddress, clientPort)
+        val jsonResponse = Gson().toJson(responseData)
+        val response = """
+            HTTP/1.1 200 OK
+            Content-Type: application/json
+            Connection: close
+
+            $jsonResponse
+        """.trimIndent()
+
+        output.write(response.toByteArray())
+        output.flush()
+    }
+
+    private fun sendImageResponse(output: OutputStream, imagePath: String) {
+        try {
+            val imageStream: InputStream = assets.open(imagePath)
+            val imageBytes = imageStream.readBytes()
+            imageStream.close()
+
+            // 정확한 형식으로 헤더 작성
+            val responseHeader = StringBuilder()
+            responseHeader.append("HTTP/1.1 200 OK\r\n")
+            responseHeader.append("Content-Type: image/png\r\n")
+            responseHeader.append("Content-Length: ${imageBytes.size}\r\n")
+            responseHeader.append("Connection: close\r\n")
+            responseHeader.append("\r\n")
+
+            Log.d(tag, "Response Header: $responseHeader")
+            Log.d(tag, "Image Bytes Length: ${imageBytes.size}")
+
+            output.write(responseHeader.toString().toByteArray(Charsets.UTF_8))
+            output.write(imageBytes)
+            output.flush()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(tag, "Error sending image response: ${e.message}")
+        }
+    }
+
+    private fun sendVideoResponse(output: OutputStream, videoPath: String) {
+        val videoStream: InputStream = assets.open(videoPath)
+        val videoBytes = videoStream.readBytes()
+        videoStream.close()
+
+        // 정확한 형식으로 헤더 작성
+        val responseHeader = StringBuilder()
+        responseHeader.append("HTTP/1.1 200 OK\r\n")
+        responseHeader.append("Content-Type: video/mp4\r\n")
+        responseHeader.append("Content-Length: ${videoBytes.size}\r\n")
+        responseHeader.append("Connection: close\r\n")
+        responseHeader.append("\r\n")
+
+        output.write(responseHeader.toString().toByteArray(Charsets.UTF_8))
+        output.write(videoBytes)
+        output.flush()
+    }
+
+    private fun sendNotFoundResponse(output: OutputStream) {
+        val response = """
+            HTTP/1.1 404 Not Found
+            Content-Type: text/html
+            Connection: close
+
+            <html>
+            <body>
+            <h1>404 Not Found</h1>
+            </body>
+            </html>
+        """.trimIndent()
+
+        output.write(response.toByteArray())
+        output.flush()
     }
 
     private fun startBroadcast() {
@@ -136,7 +270,7 @@ class HostingService : Service() {
                 val buffer = message.toByteArray()
                 val packet = DatagramPacket(buffer, buffer.size, broadcastAddress, BROADCAST_PORT)
                 socket.send(packet)
-                Log.e("Service", "Broadcast message sent: $message")
+                Log.d(tag, "Broadcast message sent: $message")
                 Thread.sleep(5000)
             } catch (e: Exception) {
                 e.printStackTrace()
@@ -186,3 +320,5 @@ class HostingService : Service() {
     }
 
 }
+
+data class ResponseData(val message: String, val clientIp: String, val clientPort: Int)
